60f6270163222ce6e8efa6a008f993e5ad1d929c
sean810 <seantashel@gmail.com>
2024-11-17 17:15:38 +0300

    Complete with passed tests

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 function App() {
   const [pepperoniIsChecked, setPepperoniIsChecked] = useState(false);
+  const [size, setSize] = useState("small");
+  const [contactInfo, setContactInfo] = useState("");
+  const [orderIsSubmitted, setOrderIsSubmitted] = useState(false);
 
   const togglePepperoni = (e) => setPepperoniIsChecked(e.target.checked);
+  const selectSize = (e) => setSize(e.target.value);
+  const updateContactField = (e) => setContactInfo(e.target.value);
+  const submitOrder = (e) => {
+    e.preventDefault();
+    setOrderIsSubmitted(true);
+  };
 
   return (
     <div>
       <h1>Place an Order</h1>
-      <form>
+
+      {/* Your Selection Text */}
+      <p>
+        Your selection: {size} {pepperoniIsChecked ? "pepperoni" : "cheese"}
+      </p>
+
+      <form onSubmit={submitOrder}>
+        {/* Size Select Element */}
+        <div>
+          <h3>Size</h3>
+          <label htmlFor="select-size">Select size: </label>
+          <select id="select-size" value={size} onChange={selectSize}>
+            <option value="small">Small</option>
+            <option value="medium">Medium</option>
+            <option value="large">Large</option>
+          </select>
+        </div>
+
+        {/* Toppings Checkbox */}
         <div>
           <h3>Toppings</h3>
           <input
@@ -20,7 +47,26 @@ function App() {
           />
           <label htmlFor="pepperoni">Add pepperoni</label>
         </div>
+
+        {/* Contact Info Text Box */}
+        <div>
+          <h3>Contact Info</h3>
+          <label htmlFor="email">Enter your email address: </label>
+          <input
+            type="text"
+            value={contactInfo}
+            id="email"
+            placeholder="email address"
+            onChange={updateContactField}
+          />
+        </div>
+
+        {/* Submit Order Button */}
+        <button type="submit">Submit Order</button>
       </form>
+
+      {/* Order Confirmation Message */}
+      {orderIsSubmitted && <h2>Thanks for your order!</h2>}
     </div>
   );
 }
